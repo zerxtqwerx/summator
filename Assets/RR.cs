e9e651b4bcf22d24d2708f43acc88a7c68c8fb0a
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-03-04 02:30:41 +0300

    a

--- Assets/RR.cs
+++ Assets/RR.cs
@@ -16,6 +16,21 @@ public class RR : MonoBehaviour
     int b = 0;
     int sum;
     string binarySum = "000000000";
+    List<bool> sum1;
+    List<bool> sum2;
+    List<bool> res;
+
+    struct SP_output
+    {
+        public bool s;
+        public bool p;
+
+        public SP_output(bool s_, bool p_)
+        {
+            s= s_;
+            p= p_;
+        }
+    }
 
     void Start()
     {
@@ -26,7 +41,8 @@ public class RR : MonoBehaviour
     public void Click()
     {
         sum = a + b;
-        Perevod();
+        Perevod(a);
+        Perevod(b);
         ChangeZnak();
         ChangeGO();
         if (!AS.isPlaying)
@@ -34,43 +50,89 @@ public class RR : MonoBehaviour
             AS.time = 0.0f;
             AS.Play();
         }
-        
-
     }
 
-    void Perevod()
+    void DecToBin(int a)
     {
-        int s = Math.Abs(sum);
-        binarySum = "";
+        int s = Math.Abs(a);
+        int count = 8;
         while(s > 0)
         {
-            binarySum = (s % 2) + binarySum;
+            if (s % 2 == 0)
+                sum2[count] = false;
+            else
+                sum2[count] = true;
+            count--;
+            s /= 2;
+        }
+    }
+    bool xor(bool a, bool b)
+    {
+        return (a || b) && (!(a && b));
+    }
+    SP_output Adder(bool ai, bool bi, bool pi_1)
+    {
+        bool pi = (ai && bi) || (ai && pi_1) || (bi && pi_1);
+        bool si = (ai || bi || pi_1) && pi_1 || (ai && bi && pi_1);
+        SP_output res = new SP_output(si, pi);
+        return res;
+    }
+
+    /*SP_output half_adder(bool a, bool b)
+    {
+        bool s = xor(a, b);
+        bool p = a && b;
+        return new SP_output(s, p);
+    }
+
+    SP_output adder(bool a, bool b, bool pi)
+    {
+        SP_output ha1 = half_adder(a, b);
+        SP_output ha2 = half_adder(pi, ha1[0]);
+        bool s = ha2.s;
+        bool p = ha1.p || ha2.p;
+        return new SP_output(s, p);
+    }*/
+
+    List<bool> Perevod(int a)
+    {
+        int s = Math.Abs(a);
+        int count = 7;
+        List<bool> res = new List<bool>();
+        while(s > 0)
+        {
+            if(s % 2 == 0)
+                res[count] = false;
+            else
+                res[count] = true;
+            count--;
             s /= 2;
         }
 
-        if(binarySum.Length < go.Count)
+        /*if(binarySum.Length < go.Count)
         {
             for(int i = binarySum.Length; i != go.Count; i++)
             {
                 binarySum = binarySum.Insert(0, "0");
             }
-        }
+        }*/
 
-        if (sum >= 0)
+        /*if (sum >= 0)
         {
             binarySum = binarySum.Insert(0, "0");
         }
         else
         {
             binarySum = binarySum.Insert(0, "1");
-        }
-        Debug.Log(binarySum);
+        }*/
+        Debug.Log(res);
+        return res;
     }
 
     public void OnReadInput1()
     {
         int.TryParse(if1.text, out int result);
-        a = result;
+        int a = result;
     }
 
     public void OnReadInput2()
