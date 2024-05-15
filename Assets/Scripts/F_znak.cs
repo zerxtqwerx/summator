b90a1496486c01e77792cdde773bac00040a0e89
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-05-15 21:49:30 +0300

    add folders

--- Assets/Scripts/F_znak.cs
+++ Assets/Scripts/F_znak.cs
@@ -5,12 +5,10 @@ using UnityEngine.UI;
 
 public class F_znak : MonoBehaviour
 {
-    public Image line;
     
     void Start()
     {
         gameObject.transform.GetChild(2).GetComponent<RawImage>().enabled = false;
         gameObject.transform.GetChild(3).GetComponent<RawImage>().enabled = false;
-        line.enabled = false;
     }
 }
